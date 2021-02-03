3f571c35f013068de054859d367ccecbcfe727c1
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-02-02 23:39:04 -0600

    get the current dates in the week

--- src/js/Common.js
+++ src/js/Common.js
@@ -45,7 +45,6 @@ class Common {
 }
 
 
-}
 
 
 
--- src/js/home.js
+++ src/js/home.js
@@ -5,16 +5,28 @@ let theUser = null;
 Main logic
 **********************************************************/
 $(document).ready(function() {
+    setUser();
+    $("#nav-item-home").addClass('active');
 
+    getCurrentDates();
+});
+
+/**********************************************************
+Loads the user object.
+
+If no user id is set in local storage, go to the login 
+page.
+**********************************************************/
+function setUser() {
     // be sure the user's id is set
     if (!isUserIdSet()) {
         window.location.href = 'login.php';
     }
 
     theUser = new User(window.localStorage.getItem('userID'));
+}
 
-    $("#nav-item-home").addClass('active');
-});
+
 
 /**********************************************************
 Checks the localStorage if the userID is set.
@@ -25,6 +37,16 @@ function isUserIdSet() {
     } else {
         return true;
     }
+}
+
+
+/**********************************************************
+Get the first and last dates in the current week.
+**********************************************************/
+function getCurrentDates() {
+    const curr     = new Date;
+    const firstday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+    const lastday  = new Date(curr.setDate(curr.getDate() - curr.getDay() + 6));
 }
 
 
